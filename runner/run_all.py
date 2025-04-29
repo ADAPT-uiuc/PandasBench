88875b85aaafc83c18bbf062c85607ecaafd453e
Stefanos Baziotis <sdi1600105@di.uoa.gr>
2025-04-29 00:53:42 -0500

    Scale factors as external files

--- runner/run_all.py
+++ runner/run_all.py
@@ -4,13 +4,14 @@ import argparse
 import os
 import sys
 import pathlib
-import scales.nbs_to_run as nbs_to_run
+import json
 
 parser = argparse.ArgumentParser(description='Run benchmarks on all notebooks in nbs_to_run.py')
 
 parser.add_argument('--lib', default='pandas', choices=["pandas", "modin", "dask", "koalas"], type=str, help='The Pandas API alternative to use. Pandas is used by default.')
 parser.add_argument('--add_on', choices=['dias'], type=str, help='Add on library to include (like Dias). By default it uses no additional library.')
 parser.add_argument('--num_cores', default=0, type=int, metavar='NUM_CORES', help='Pass additional argument for the library.')
+parser.add_argument('--scale_file', default='scales/nbs_to_run_default.json', type=str, help='JSON file that contains the scale factor for each notebook. By default we use the file that uses a scale factor of 1.0.')
 parser.add_argument('--scale_input_and_exit', action='store_true', help='Scales the benchmark. The benchmark will not properly run if the input has never been scaled.')
 
 args = parser.parse_args()
@@ -56,28 +57,21 @@ should_run_nb_body = not args.scale_input_and_exit
 
 prefix = str(pathlib.Path('../notebooks').resolve())
 
-good = []
-bad = []
-start = 0
-end = len(nbs_to_run.notebooks)
+with open(args.scale_file, 'r') as fp:
+  scale_d = json.load(fp)
 
-nbs_to_ignore = set()
+nb_scales = []
+for pair in scale_d['notebooks']:
+  nb_scales.append((pair['nb'], pair['scale-factor']))
+### END FOR ###
+
 try:
-  for i, (nb, scale_factor) in enumerate(nbs_to_run.notebooks):
-    scale_factor = 0.001
-    if nb in nbs_to_ignore:
-      continue
+  for i, (nb, scale_factor) in enumerate(nb_scales):
     kernel_user = nb.split('/')[0]
     kernel_slug = nb.split('/')[1]
     full_path = prefix+"/"+nb
     print(i + 1)
     print(f"--- RUNNING: {kernel_user}/{kernel_slug}")
     succ = run_nb.run_nb_paper(full_path, args.lib if should_run_nb_body else '', args.num_cores, add_on, scale_factor, args.scale_input_and_exit)
-    if should_run_nb_body:
-      if succ:
-        res = subprocess.run(["mv", "stats.json", f"stats/{kernel_user}_{kernel_slug}.json"])
-        good.append(nb)
-      else:
-        bad.append(nb)
 except KeyboardInterrupt:
   pass
